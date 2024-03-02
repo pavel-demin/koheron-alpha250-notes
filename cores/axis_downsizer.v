dcfc82509634bf71b3467e6139637b01975667a0
Pavel Demin <pavel-demin@outlook.com>
2024-03-02 22:42:57 +0100

    simplify axis_downsizer

--- cores/axis_downsizer.v
+++ cores/axis_downsizer.v
@@ -26,30 +26,20 @@ module axis_downsizer #
 
   localparam integer RATIO = S_AXIS_TDATA_WIDTH / M_AXIS_TDATA_WIDTH;
   localparam integer CNTR_WIDTH = RATIO > 1 ? $clog2(RATIO) : 1;
-  localparam integer DATA_WIDTH = S_AXIS_TDATA_WIDTH - M_AXIS_TDATA_WIDTH;
 
-  reg [DATA_WIDTH-1:0] int_data_reg, int_data_next;
-  reg [CNTR_WIDTH-1:0] int_cntr_reg, int_cntr_next;
+  reg [CNTR_WIDTH-1:0] int_cntr_reg;
 
-  wire [M_AXIS_TDATA_WIDTH-1:0] int_data_mux [RATIO-2:0];
-  wire [M_AXIS_TDATA_WIDTH-1:0] int_data_wire;
-  wire [CNTR_WIDTH-1:0] int_sel_wire;
-  wire int_comp_wire, int_valid_wire, int_ready_wire;
+  wire [M_AXIS_TDATA_WIDTH-1:0] int_data_mux [RATIO-1:0];
+  wire int_last_wire, int_ready_wire;
 
   genvar j;
 
-  assign int_sel_wire = cfg_data[CNTR_WIDTH-1:0] - int_cntr_reg;
-
-  assign int_comp_wire = |int_cntr_reg;
-
-  assign int_valid_wire = int_comp_wire | s_axis_tvalid;
-
-  assign int_data_wire = int_comp_wire ? int_data_mux[int_sel_wire] : s_axis_tdata[M_AXIS_TDATA_WIDTH-1:0];
+  assign int_last_wire = int_cntr_reg == cfg_data[CNTR_WIDTH-1:0];
 
   generate
-    for(j = 0; j < RATIO - 1; j = j + 1)
+    for(j = 0; j < RATIO; j = j + 1)
     begin : WORDS
-      assign int_data_mux[j] = int_data_reg[j*M_AXIS_TDATA_WIDTH+:M_AXIS_TDATA_WIDTH];
+      assign int_data_mux[j] = s_axis_tdata[j*M_AXIS_TDATA_WIDTH+:M_AXIS_TDATA_WIDTH];
     end
   endgenerate
 
@@ -57,30 +47,11 @@ module axis_downsizer #
   begin
     if(~aresetn)
     begin
-      int_data_reg <= {(DATA_WIDTH){1'b0}};
       int_cntr_reg <= {(CNTR_WIDTH){1'b0}};
     end
-    else
+    else if(s_axis_tvalid & int_ready_wire)
     begin
-      int_data_reg <= int_data_next;
-      int_cntr_reg <= int_cntr_next;
-    end
-  end
-
-  always @*
-  begin
-    int_data_next = int_data_reg;
-    int_cntr_next = int_cntr_reg;
-
-    if(int_comp_wire & int_ready_wire)
-    begin
-      int_cntr_next = int_cntr_reg - 1'b1;
-    end
-
-    if(s_axis_tvalid & ~int_comp_wire & int_ready_wire)
-    begin
-      int_cntr_next = cfg_data[CNTR_WIDTH-1:0];
-      int_data_next = s_axis_tdata[S_AXIS_TDATA_WIDTH-1:M_AXIS_TDATA_WIDTH];
+      int_cntr_reg <= int_last_wire ? {(CNTR_WIDTH){1'b0}} : int_cntr_reg + 1'b1;
     end
   end
 
@@ -88,10 +59,10 @@ module axis_downsizer #
     .DATA_WIDTH(M_AXIS_TDATA_WIDTH)
   ) buf_0 (
     .aclk(aclk), .aresetn(aresetn),
-    .in_data(int_data_wire), .in_valid(int_valid_wire), .in_ready(int_ready_wire),
+    .in_data(int_data_mux[int_cntr_reg]), .in_valid(s_axis_tvalid), .in_ready(int_ready_wire),
     .out_data(m_axis_tdata), .out_valid(m_axis_tvalid), .out_ready(m_axis_tready)
   );
 
-  assign s_axis_tready = ~int_comp_wire & int_ready_wire;
+  assign s_axis_tready = int_last_wire & int_ready_wire;
 
 endmodule
